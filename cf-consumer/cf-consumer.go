5f33960f23b83a18e5e535aae9314b24f4b5f638
Derek Tracy <tracyde@gmail.com>
2015-03-30 17:56:33 -0400

    made work without being bound and show status of bind

--- cf-consumer/cf-consumer.go
+++ cf-consumer/cf-consumer.go
@@ -15,71 +15,76 @@ import (
 type Status struct {
 	InstanceProcessed int
 	InstanceIndex     int
+	AMQP              bool
+	REDIS             bool
 }
 
-func failOnError(err error, msg string) {
+func logOnError(err error, msg string) {
 	if err != nil {
-		log.Panicf("%s: %s", msg, err)
+		log.Printf("%s: %s", msg, err)
 	}
 }
 
 func main() {
-	appEnv, _ := cfenv.Current()
-	services, err := appEnv.Services.WithTag("rabbitmq")
-	failOnError(err, "Unable to find bound CloudAMQP instance")
-
-	rabbitmq := services[0]
-	rabbitmqURI := rabbitmq.Credentials["uri"]
-	conn, err := amqp.Dial(rabbitmqURI)
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"task_queue", // name
-		true,         // durable
-		false,        // delete when unused
-		false,        // exclusive
-		false,        // no-wait
-		nil,          // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	err = ch.Qos(
-		3,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
-	failOnError(err, "Failed to set QoS")
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		false,  // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	// start the work
 	status := Status{}
+	appEnv, _ := cfenv.Current()
 	status.InstanceIndex = appEnv.Index
-	go func() {
-		for d := range msgs {
-			fmt.Printf("Received a message: %s\n", d.Body)
-			d.Ack(false)
-			time.Sleep(100 * time.Millisecond)
-			status.InstanceProcessed++
-		}
-	}()
+	services, err := appEnv.Services.WithTag("rabbitmq")
+	logOnError(err, "Unable to find bound CloudAMQP service")
+	if err == nil {
+		// enable rabbitmq communication
+		status.AMQP = true
+
+		rabbitmq := services[0]
+		rabbitmqURI := rabbitmq.Credentials["uri"]
+		conn, err := amqp.Dial(rabbitmqURI)
+		logOnError(err, "Failed to connect to RabbitMQ")
+		defer conn.Close()
+
+		ch, err := conn.Channel()
+		logOnError(err, "Failed to open a channel")
+		defer ch.Close()
+
+		q, err := ch.QueueDeclare(
+			"task_queue", // name
+			true,         // durable
+			false,        // delete when unused
+			false,        // exclusive
+			false,        // no-wait
+			nil,          // arguments
+		)
+		logOnError(err, "Failed to declare a queue")
+
+		err = ch.Qos(
+			3,     // prefetch count
+			0,     // prefetch size
+			false, // global
+		)
+		logOnError(err, "Failed to set QoS")
+
+		msgs, err := ch.Consume(
+			q.Name, // queue
+			"",     // consumer
+			false,  // auto-ack
+			false,  // exclusive
+			false,  // no-local
+			false,  // no-wait
+			nil,    // args
+		)
+		logOnError(err, "Failed to register a consumer")
+
+		// start the work
+		go func() {
+			for d := range msgs {
+				fmt.Printf("Received a message: %s\n", d.Body)
+				d.Ack(false)
+				time.Sleep(100 * time.Millisecond)
+				status.InstanceProcessed++
+			}
+		}()
+	}
 
 	m := martini.Classic()
-	// render html templates from templates directory
 	m.Use(render.Renderer())
 
 	m.Get("/", func(r render.Render) {
