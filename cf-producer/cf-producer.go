117fe4bbdf0e7cf4835d988e33c45de5031a9de0
Derek Tracy <tracyde@gmail.com>
2015-03-31 10:48:20 -0400

    added status page and run without being bound to rabbitmq

--- cf-producer/cf-producer.go
+++ cf-producer/cf-producer.go
@@ -8,18 +8,31 @@ import (
 
 	"github.com/Pallinder/go-randomdata"
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 	"github.com/streadway/amqp"
 
 	cfenv "github.com/cloudfoundry-community/go-cfenv"
 )
 
-func failOnError(err error, msg string) {
+var ch *amqp.Channel
+
+type Status struct {
+	InstanceProcessed int
+	InstanceIndex     int
+	AMQP              bool
+}
+
+func logOnError(err error, msg string) {
 	if err != nil {
-		log.Panicf("%s: %s", msg, err)
+		log.Printf("%s: %s", msg, err)
 	}
 }
 
-func publishMessage(ch *amqp.Channel) {
+func publishMessage(ch *amqp.Channel, status *Status) {
+	if ch == nil {
+		log.Printf("Can not publish to nil amqp.Channel")
+		return
+	}
 	body := fmt.Sprintf("Received email from <%s>", randomdata.Email())
 	err := ch.Publish(
 		"",           // exchange
@@ -31,46 +44,58 @@ func publishMessage(ch *amqp.Channel) {
 			ContentType:  "text/plain",
 			Body:         []byte(body),
 		})
-	failOnError(err, "Failed to publish a message")
+	logOnError(err, "Failed to publish a message")
+	status.InstanceProcessed++
 }
 
 func main() {
+	status := Status{}
 	appEnv, _ := cfenv.Current()
+	status.InstanceIndex = appEnv.Index
 	services, err := appEnv.Services.WithTag("rabbitmq")
-	failOnError(err, "Unable to find bound CloudAMQP instance")
+	logOnError(err, "Unable to find bound CloudAMQP instance")
+	if err == nil {
+		// enable rabbitmq communication
+		status.AMQP = true
+		rabbitmq := services[0]
+		rabbitmqURI := rabbitmq.Credentials["uri"]
+		conn, err := amqp.Dial(rabbitmqURI)
+		logOnError(err, "Failed to connect to RabbitMQ")
+		defer conn.Close()
 
-	rabbitmq := services[0]
-	rabbitmqURI := rabbitmq.Credentials["uri"]
-	conn, err := amqp.Dial(rabbitmqURI)
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+		ch, err := conn.Channel()
+		logOnError(err, "Failed to open a channel")
+		defer ch.Close()
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	// start publishing
-	go func() {
-		for {
-			publishMessage(ch)
-			time.Sleep(100 * time.Millisecond)
-		}
-	}()
+		// start publishing
+		go func() {
+			for {
+				publishMessage(ch, &status)
+				time.Sleep(100 * time.Millisecond)
+			}
+		}()
+	}
 
 	m := martini.Classic()
+	m.Use(render.Renderer())
+
 	m.Post("/publish/:amount", func(params martini.Params) string {
+		if !status.AMQP {
+			log.Panicf("Not bound to CloudAMQP instance")
+		}
 		x, err := strconv.Atoi(params["amount"])
 		if err != nil {
-			log.Printf("Unable to convert %s to int")
+			log.Panicf("Unable to convert %s to int")
 		}
 		for i := 0; i < x; i++ {
-			publishMessage(ch)
+			publishMessage(ch, &status)
 		}
 		return "Published " + params["amount"] + " messages\n"
 	})
-	m.Get("/", func(params martini.Params) string {
-		return "Messages are being published"
-	})
-	m.Run()
 
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "status", status)
+	})
+
+	m.Run()
 }
